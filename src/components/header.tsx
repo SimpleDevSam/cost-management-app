3167a05587a1fd12aa581d62a199a0d75c09a172
Samuel Maia <samuel.assis.ext@acelen.com>
2025-09-09 11:40:11 -0300

    adding clerk auth updating icos and title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,26 @@
+'use client'
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
 import { AlignJustify } from "lucide-react";
 
-interface HeaderProps {
-  companyName: string;
-}
 
-export function Header({ companyName }: HeaderProps) {
+export function Header() {
+  const {user} = useUser()
+  
   return (
     <header className="w-full p-4 border-b bg-background text-gray-900">
       <div className="flex flex-row justify-center items-center gap-2">
-        <AlignJustify />
-        <h1 className="text-lg  font-bold text-center">{companyName} App</h1>
+        <SignedIn>
+          <AlignJustify />
+          <h1 className="text-lg  font-bold text-center">{user?.primaryEmailAddress?.emailAddress}</h1>
+        </SignedIn>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+        </SignedIn>
       </div>
     </header>
   );
